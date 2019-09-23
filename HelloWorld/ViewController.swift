1b374d5ebe519313fdaf6a32e66543ac779e5253
samer Mobile <iphonei16@hotmail.com>
2019-09-24 00:00:53 +0300

    first commit

--- HelloWorld/ViewController.swift
+++ HelloWorld/ViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var helloLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
 
+    @IBAction func Hello(_ sender: Any) {
+
+        print("hello")
+        helloLabel.text="Hello World"
+    }
 }
 
